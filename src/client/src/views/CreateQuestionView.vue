34dd2f802473c1a4748c69505593e651e57cc73d
Georg Richter <georg.richter@stud.htw-dresden.de>
2025-06-16 11:25:52 +0200

    Start CreateQuestion und Quiz

---
+++ src/client/src/views/CreateQuestionView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="createQuestion">
+    <h1>Hier kommt das Frage bearbeiten hin :P</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/client/src/views/CreateQuizView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="createQuiz">
+    <h1>Hier kommt das Quiz bearbeiten hin :P</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
