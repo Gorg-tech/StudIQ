76075c01d14e8840bdf4bb37baa756521730f337
Georg Richter <georg.richter@stud.htw-dresden.de>
2025-06-23 12:28:11 +0200

    settings angepasst

--- src/server/config/settings.py
+++ src/server/config/settings.py
@@ -41,16 +41,22 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'accounts',
     'quizzes',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [ #Hier Adresse von Client eingeben (nur für Development)
+    "http://127.0.0.1:8000/admin/",
 ]
 
 ROOT_URLCONF = 'config.urls'
