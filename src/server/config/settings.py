c5bb27f9d4bf41abd236f0cf74ee1834c6a6a810
Georg Richter <georg.richter@stud.htw-dresden.de>
2025-06-23 12:43:45 +0200

    settings korrigiert

--- src/server/config/settings.py
+++ src/server/config/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [ #Hier Adresse von Client eingeben (nur für Development)
-    "http://127.0.0.1:8000/admin/",
+    "http://localhost:5173/search",
 ]
 
 ROOT_URLCONF = 'config.urls'
