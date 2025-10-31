668aa0001d3e1db0dd0afaa9680c1688cd5e1003
BUK <132343254+git-blame-BUK@users.noreply.github.com>
2025-10-31 21:08:30 +0100

    endpoit router quizId, korrekte api calls, debug ing fuer Laden der Quizzes , Statitik und Lernset noch nicht funktinal (#170)
    Co-authored-by: buk <s90814@htw-dresden.de>

--- src/client/src/router/index.js
+++ src/client/src/router/index.js
@@ -65,7 +65,7 @@ const router = createRouter({
       component: QuizResultView,
     },
     { 
-      path: '/quiz',
+      path: '/quiz/:quizId',
       name: 'quiz',
       component: QuizView,
     },
