14469c53410d2ee211e9044406e99433d091a69c
Vincent Karmanoczki <vincent.karmanoczki@stud.htw-dresden.de>
2025-11-10 23:34:56 +0100

    ProfileView now receiving data from server

--- src/client/src/services/user.js
+++ src/client/src/services/user.js
@@ -4,3 +4,11 @@ import { API_ENDPOINTS } from './api/endpoints';
 export async function getSelfUser() {
   return apiClient.get(API_ENDPOINTS.USER.ME);
 }
+
+export async function getSelfUserStats() {
+  return apiClient.get(API_ENDPOINTS.USER.MY_STATS);
+}
+
+export async function getSelfUserStreaks() {
+  return apiClient.get(API_ENDPOINTS.USER.MY_STREAKS);
+}
