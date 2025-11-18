492d5f7791f4750d77826274ce091933beda7925
Vincent Karmanoczki <vincent.karmanoczki@stud.htw-dresden.de>
2025-11-18 02:22:03 +0100

    Readded streak field

--- src/server/accounts/models.py
+++ src/server/accounts/models.py
@@ -12,6 +12,7 @@ class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     role = models.CharField(max_length=10, choices=UserRole.choices, default=UserRole.STUDENT)
     iq_level = models.IntegerField(default=0)
+    streak = models.IntegerField(default=0)
     correct_answers = models.IntegerField(default=0)
     wrong_answers = models.IntegerField(default=0)
     solved_quizzes = models.IntegerField(default=0)
