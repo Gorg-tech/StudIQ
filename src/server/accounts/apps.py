e1d4d3ebb22f2189f2e2d704c103708f6f15bbb9
Vincent Karmanoczki <vincent.karmanoczki@stud.htw-dresden.de>
2025-12-01 23:46:05 +0100

    Added docstrings to accounts/apps.py

--- src/server/accounts/apps.py
+++ src/server/accounts/apps.py
@@ -1,6 +1,15 @@
+"""
+Django App Configuration for accounts.
+
+This file defines the django app configuration.
+"""
+
 from django.apps import AppConfig
 
-
 class AccountsConfig(AppConfig):
+    """
+    Default configuration for the app.
+    Sets default field type and the name of the app.  
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
