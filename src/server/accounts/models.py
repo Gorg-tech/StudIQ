e908c0fd0b3e4bab5f1efd0facb8945daa478552
Vincent Karmanoczki <vincent.karmanoczki@stud.htw-dresden.de>
2025-12-01 23:45:55 +0100

    Added docstrings to accounts/models.py

--- src/server/accounts/models.py
+++ src/server/accounts/models.py
@@ -1,14 +1,33 @@
+"""
+Database Model for account related objects.
+
+This file defines account-related entites, their attribute fields and their relationships.
+"""
+
 import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from quizzes.models import Studiengang
 
 class UserRole(models.TextChoices):
+    """
+    A datatype representing the role of the user.
+
+    Options:
+        STUDENT, MODERATOR, LECTURER
+    """
     STUDENT = 'STUDENT', 'Student'
     MODERATOR = 'MODERATOR', 'Moderator'
     LECTURER = 'LECTURER', 'Lecturer'
 
 class User(AbstractUser):
+    """
+    Represents a StudIQ account that has to be created to use StudIQ.
+
+    Attributes:
+        id, role, iq_level, streak, correct_answers,
+        wrong_answers, solved_quizzes, registration_date, studiengang
+    """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     role = models.CharField(max_length=10, choices=UserRole.choices, default=UserRole.STUDENT)
     iq_level = models.IntegerField(default=0)
@@ -18,18 +37,27 @@ class User(AbstractUser):
     solved_quizzes = models.IntegerField(default=0)
     registration_date = models.DateField(auto_now_add=True)
     # last_login is already part of AbstractUser
-    
+
     # Will be linked to Studiengang later
     studiengang = models.ForeignKey(Studiengang, on_delete=models.SET_NULL, null=True, blank=True)
-    
+
     def __str__(self):
         return self.username
-    
+
 class StudyDay(models.Model):
+    """
+    Represents a day at which the user had studied.
+
+    Attributes:
+        user, date
+    """
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='study_days')
     date = models.DateField()
-    
+
     class Meta:
+        """
+        Defines primary key behaviour for user,date and orders by date descending.
+        """
         unique_together = ('user', 'date')  # Entry per user per day should be unique
         ordering = ['-date']
 
