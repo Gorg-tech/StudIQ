82ac18241bb5758586c9b34926173f8bd3c0239c
minadr0id <carmina.langer@gmail.com>
2025-12-15 18:22:08 +0100

    testcases for integration

--- src/server/tests/modul/test_modul_acc.py
+++ src/server/tests/modul/test_modul_acc.py
@@ -1,34 +1,69 @@
-
-import os
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-django.setup()
-
 from django.test import TestCase
 from django.db.utils import IntegrityError
 from django.contrib.auth import get_user_model
+from accounts.serializers import RegisterSerializer, LoginSerializer
+from django.contrib.auth import authenticate
 
 User = get_user_model()
 
-class TestRegistration(TestCase):
+class TestAccounts(TestCase):
     def setUp(self):
-        self.user = User.objects.create(username="testuser", password="testpass")
+        self.user = User.objects.create(username="testuser", password="testpass12345!?")
 
-    def tearDown(self):
-        return super().tearDown()
+    def test_creating_new_valid_user_suceeds(self):
+        data={
+            "username": "newuser",
+            "password": "StrongPassword123!#",
+            "studiengang" : "Informatik"
+        }
 
-    def test_registration_with_missing_username(self):
-        """Ensure that registration fails with missing username"""
-        with self.assertRaises(IntegrityError):
-            User.objects.create(username=None, password="testpass")
+        serializer=RegisterSerializer(data=data)
+        self.assertTrue(serializer.is_valid(), serializer.errors)
+        user=serializer.save()
 
-    def test_registration_with_missing_password(self):
-        """Ensure that registration fails with missing password"""
-        with self.assertRaises(IntegrityError):
-            User.objects.create(username="testuser", password=None)
+    def test_login_with_valid_credentials_succeeds(self):
+        data={ 
+            "username": self.user.username,
+            "password": "testpass12345!?"
+            }
+        serializer = LoginSerializer(data=data)
+        self.assertTrue(serializer.is_valid(), serializer.errors)
 
-    def test_name_not_unique(self):
-        """Ensure that registration fails when username is not unique"""
-        with self.assertRaises(IntegrityError):
-            User.objects.create(username="testuser", password="anotherpass")
+    def test_login_with_invalid_credentials_fails(self):
+        data={ 
+            "username": self.user.username,
+            "password": "wrongpassword"
+            }
+        
+        serializer = LoginSerializer(data=data)
+        user=authenticate(username=data["username"], password=data["password"])
+        self.assertIsNone(user)
+        
+    def test_missing_stdiengang_fails(self):
+        data={
+            "username": "anotheruser",
+            "password": "anotherpass"
+        }
+        serializer=RegisterSerializer(data=data)
+        self.assertFalse(serializer.is_valid(), serializer.errors)
+
+    def test_creating_user_with_existing_username_fails(self):
+
+        data={
+            "username": self.user.username,
+            "password": self.user.password,
+            "studiengang" : "Informatik"
+        }
+
+        serializer=RegisterSerializer(data=data)
+        self.assertFalse(serializer.is_valid(), serializer.errors)
+
+    def test_warning_on_weak_password(self):
+        data={
+            "username": "weakuser",
+            "password": "123",
+            "studiengang" : "Informatik"
+        }
+
+        serializer = RegisterSerializer(data=data)
+        self.assertFalse(serializer.is_valid(), serializer.errors)
