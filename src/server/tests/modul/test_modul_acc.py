cffb9c8a8bea3d027dc85ca242d61eebab643c41
minadr0id <carmina.langer@gmail.com>
2026-01-06 12:12:20 +0100

    modified/ corrected modul tests

--- src/server/tests/modul/test_modul_acc.py
+++ src/server/tests/modul/test_modul_acc.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.db.utils import IntegrityError
 from django.contrib.auth import get_user_model
-from accounts.serializers import RegisterSerializer, LoginSerializer
+from accounts.serializers import RegisterSerializer, LoginSerializer, FriendRequestSerializer
+from accounts.models import FriendRequest
 from django.contrib.auth import authenticate
 
 User = get_user_model()
@@ -67,3 +68,15 @@ class TestAccounts(TestCase):
 
         serializer = RegisterSerializer(data=data)
         self.assertFalse(serializer.is_valid(), serializer.errors)
+
+
+    def test_friendrequest_to_unknown_user_fails(self):
+        data={
+            "from_user": self.user.username,
+            "to_user": "unknownuser"
+        }
+
+        serializer=FriendRequestSerializer(data=data)
+        self.assertFalse(serializer.is_valid(), serializer.errors)
+
+  
