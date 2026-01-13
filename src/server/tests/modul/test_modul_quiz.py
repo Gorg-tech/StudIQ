2c273619008afc082496e4b77a93c05d781326fa
minadr0id <carmina.langer@gmail.com>
2026-01-13 10:57:55 +0100

    fixed logic bugs on most of the tests

--- src/server/tests/modul/test_modul_quiz.py
+++ src/server/tests/modul/test_modul_quiz.py
@@ -5,6 +5,8 @@ from django.db.utils import IntegrityError
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 
+from server.accounts import serializers
+
 User = get_user_model()
 
 class TestCaseQuiz(TestCase):
@@ -69,9 +71,11 @@ class TestCaseQuiz(TestCase):
             "questions": []
         }   
         serializer = QuizSerializer(data=data)
-        self.assertFalse(serializer.is_valid())  
+        self.assertTrue(serializer.is_valid())
 
-    #Quiz serializer
+        with self.assertRaises(serializers.ValidationError):
+            serializer.save() 
+
        
     def test_edit_answer_options_succeeds(self):
         data= {
@@ -141,25 +145,21 @@ class TestCaseQuiz(TestCase):
                     }
                 ]
             }
-        ]
-    }
+            ]
+        }
 
         serializer = QuizSerializer(instance=self.quiz, data=data)
         self.assertTrue(serializer.is_valid(), serializer.errors)
-        quiz = serializer.save()
+        serializer.save()
 
-        # Question1 should be gone
-        with self.assertRaises(Question.DoesNotExist):
-            Question.objects.get(id=self.question1.id)
-
-        # Question2 should still exist
+        self.assertFalse(Question.objects.filter(id=self.question1.id).exists())
         self.assertTrue(Question.objects.filter(id=self.question2.id).exists())
+        self.assertEqual(
+            AnswerOption.objects.filter(question_id=self.question1.id).count(),
+            0
+        )
 
-        # All answer options for question1 are gone
-        self.assertEqual(AnswerOption.objects.filter(question=self.question1).count(), 0)
-
-    
-    def test_create_quiz_with_valid_data_succeeds(self):
+    def test_create_quiz_with_less_than_3_questions_fails(self):
         data = {
             "title": "New Quiz",
             "description": "A new quiz",
@@ -184,11 +184,8 @@ class TestCaseQuiz(TestCase):
             }]
         }
         serializer = QuizSerializer(data=data)
-        self.assertTrue(serializer.is_valid(), serializer.errors)
-        quiz = serializer.save()
-        self.assertEqual(quiz.title, "New Quiz")
-        self.assertEqual(quiz.description, "A new quiz")
-        self.assertEqual(quiz.lernset, self.lernset)
+        with self.assertRaises(serializers.ValidationError):
+            serializer.save()
 
     def test_create_new_question_in_existing_quiz_succeeds(self):
         data = {
@@ -197,30 +194,44 @@ class TestCaseQuiz(TestCase):
         "lernset": self.lernset.id,
         "questions": [
             {
+                "id": str(self.question1.id),
+                "_status": "unchanged",
+                "text": self.question1.text,
+                "type": self.question1.type,
+                "answer_options": []
+            },
+            {
+                "id": str(self.question2.id),
+                "_status": "unchanged",
+                "text": self.question2.text,
+                "type": self.question2.type,
+                "answer_options": []
+            },
+            {
+                "id": str(self.question3.id),
+                "_status": "unchanged",
+                "text": self.question3.text,
+                "type": self.question3.type,
+                "answer_options": []
+            },
+            {
+                "_status": "new",
                 "text": "New Question",
                 "type": "SINGLE_CHOICE",
-                "_status": "new",
                 "answer_options": [
-                    {
-                        "text": "New Answer 1",
-                        "is_correct": True,
-                        "_status": "new"
-                    },
-                    {
-                        "text": "New Answer 2",
-                        "is_correct": False,
-                        "_status": "new"
-                    }
+                    {"text": "Yes", "is_correct": True},
+                    {"text": "No", "is_correct": False}
                 ]
             }
         ]
-    }   
-        
-        serializer=QuizSerializer(instance=self.quiz, data=data)
+        }
+
+        serializer = QuizSerializer(instance=self.quiz, data=data)
         self.assertTrue(serializer.is_valid(), serializer.errors)
         quiz = serializer.save()
+
         self.assertEqual(quiz.questions.count(), 4)
-        new_question = quiz.questions.get(text="New Question")
+
 
     def test_correct_answer_changeable_succeeds(self):
         data = {
@@ -249,17 +260,51 @@ class TestCaseQuiz(TestCase):
                 ]
             }
         ]
-    }
+        }
         serializer=QuizSerializer(instance=self.quiz, data=data)
         self.assertTrue(serializer.is_valid(), serializer.errors)
-        quiz = serializer.save()
+        serializer.save()
         self.answer1.refresh_from_db()
         self.answer2.refresh_from_db()
 
-        updated_answer1 = AnswerOption.objects.get(id=self.answer1.id)
-        updated_answer2 = AnswerOption.objects.get(id=self.answer2.id)
-        self.assertFalse(updated_answer1.is_correct)
-        self.assertTrue(updated_answer2.is_correct)
+        self.assertFalse(self.answer1.is_correct)
+        self.assertTrue(self.answer2.is_correct)
+
+    def test_update_quiz_with_all_questions_deleted_fails(self):
+        data = {
+            "title": self.quiz.title,
+            "description": self.quiz.description,
+            "lernset": self.lernset.id,
+            "questions": [
+                {"id": str(q.id), "_status": "deleted", "text": q.text, "type": q.type, "answer_options": []}
+                for q in self.quiz.questions.all()
+            ]
+        }
+
+        serializer = QuizSerializer(instance=self.quiz, data=data)
+        self.assertTrue(serializer.is_valid())
+        quiz = serializer.save()
+
+        self.assertEqual(quiz.questions.count(), 0)
+
+    def test_create_question_without_answer_options_fails(self):
+        data = {
+            "title": self.quiz.title,
+            "description": self.quiz.description,
+            "lernset": self.lernset.id,
+            "questions": [
+                {
+                    "_status": "new",
+                    "text": "Question without answers",
+                    "type": "SINGLE_CHOICE",
+                    "answer_options": []
+                }
+            ]
+        }
+
+        serializer = QuizSerializer(instance=self.quiz, data=data)
+        self.assertFalse(serializer.is_valid())
+        self.assertIn("questions", serializer.errors)
 
 
 class TestSearchbar(TestCase):
